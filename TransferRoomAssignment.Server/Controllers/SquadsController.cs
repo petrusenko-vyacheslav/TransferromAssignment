b2f5b9dafd209cc144731400fb85cc39f0b54613
Admin <Admin@DESKTOP-HDI2EDB>
2025-07-06 01:10:28 +0300

    Add project files.

---
+++ TransferRoomAssignment.Server/Controllers/SquadsController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TransferRoomAssignment.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SquadsController : ControllerBase
+    {
+        public SquadsController()
+        {
+        }
+    }
+}
